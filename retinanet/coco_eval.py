72bf4277685c09d6ad3a2ad699aa698d9242c2b0
DavidCWQ <caowenqi4812@163.com>
2025-05-02 00:23:15 +0200

    chore: comments update

--- retinanet/coco_eval.py
+++ retinanet/coco_eval.py
@@ -35,7 +35,7 @@ def evaluate_coco(dataset, model, threshold=0.05):
                 boxes[:, 3] -= boxes[:, 1]
 
                 # compute predicted labels and scores
-                #for box, score, label in zip(boxes[0], scores[0], labels[0]):
+                # for box, score, label in zip(boxes[0], scores[0], labels[0]):
                 for box_id in range(boxes.shape[0]):
                     score = float(scores[box_id])
                     label = int(labels[box_id])
